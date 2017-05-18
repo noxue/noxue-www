c8588bd43f59f5666066aea22f153069c79a7f01
noxue <yes@noxue.com>
2017-05-19 02:02:00 +0800

    完成课时安排  课程大纲

--- public/js/php.js
+++ public/js/php.js
@@ -21,22 +21,29 @@ $(document).ready(function() {
                 setTimeout(function(){
                     $('.kcap-2').addClass('animated wobble');
                 }, 1000);
+            } else if(3==index) {
+                $(".section-31").addClass('animated fadeInLeft');
+                $(".section-32").addClass('animated fadeInRight');
+                $(".section-33").addClass('animated rotateIn');
             }
         },
         onLeave: function(index, nextIndex, direction) {
             var section = $(this);
 
             if(1==index) {
+                // $('.html').removeClass('flipInX');
                 $('.html').addClass("animated flipOutX");
                 setTimeout(function(){
                     $('.html').removeClass('flipOutX');
                 }, 1000);
 
+                // $('.php').removeClass('fadeInLeft');
                 $('.phpmysql>.php').addClass("animated fadeOutLeft");
                 setTimeout(function(){
                     $('.phpmysql>.php').removeClass('fadeOutLeft');
                 }, 1000);
 
+                // $('.laravel').removeClass('fadeInRight');
                 $('.phpmysql>.laravel').addClass("animated fadeOutRight");
                 setTimeout(function(){
                     $('.phpmysql>.laravel').removeClass('fadeOutRight');
@@ -47,7 +54,26 @@ $(document).ready(function() {
             } else if(2==index) {
                 $('.kcap-1').removeClass('flip');
                 $('.kcap-2').removeClass('wobble');
+            } else if(3==index) {
+                // $(".section-31").removeClass('fadeInLeft');
+                $('.section-31').addClass("animated fadeOutRight");
+                setTimeout(function(){
+                    $('.section-31').removeClass('fadeOutRight');
+                }, 1000);
+
+                // $('.section-32').removeClass('fadeInRight');
+                $('.section-32').addClass("animated fadeOutLeft");
+                setTimeout(function(){
+                    $('.section-32').removeClass('fadeOutLeft');
+                }, 1000);
+
+                // $('.section-33').removeClass('rotateIn');
+                $('.section-33').addClass("animated rotateOut");
+                setTimeout(function(){
+                    $('.section-33').removeClass('rotateOut');
+                }, 1000);
             }
         }
     });
 });
+
--- resources/views/page/php.blade.php
+++ resources/views/page/php.blade.php
@@ -11,7 +11,6 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.css" />
     <link rel="stylesheet" href="{{asset('css/php.css')}}" />
 
-
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.9.4/jquery.fullpage.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.9.4/jquery.fullpage.extensions.min.js"></script>
@@ -46,35 +45,45 @@
             <img src="{{asset('/images/php/section2.jpg')}}" />
         </div>
         <div class="container">
+            <h1>课程大纲</h1>
             <div class="kcap-1 table-responsive">
                 <table class="table table-bordered table-hover">
                     <thead>
-                    <tr style="background-color:#FF8F00; color:#eee;"><th>课程</th><th>课时</th><th>内容</th></tr>
+                    <tr style="background-color:#FF8F00; color:#eee;"><th style="width:200px;">课程</th><th>内容</th></tr>
                     </thead>
                     <tbody>
                     <tr class="warning">
-                        <th>html5</th><th>1周</th><th>内容简介</th>
+                        <th>html/html5</th><td>让学员熟悉代码，学会html标签和html5新特性。熟练使用html5标签编写出网页</td>
                     </tr>
                     <tr class="info">
-                        <th>css3</th><th>1周</th><th>内容简介</th>
+                        <th>css3</th><td>css3让网页更加漂亮，熟练使用css3样式和css3的各种高级用法。此时学员能制作出任何常见的静态页面效果</td>
                     </tr>
                     <tr class="success">
-                        <th>javascript</th><th>1周</th><th>内容简介</th>
+                        <th>javascript</th><td>javascript实现动态效果，学会之后，可以实现网页上一些酷炫的动态效果，如：下拉菜单，图片轮播等。</td>
+                    </tr>
+                    <tr class="warning">
+                        <th>ajax</th><td>无刷新更新网页内容技术，让网页不用刷新就可以与服务端交互数据</td>
                     </tr>
                     <tr class="danger">
-                        <th>jquery</th><th>1周</th><th>内容简介</th>
+                        <th>jquery</th><td>能让我们减少工作量，用更少的代码做更多的事情。</td>
                     </tr>
                     <tr class="warning">
-                        <th>bootstrap</th><th>1周</th><th>内容简介</th>
-                    </tr>
-                    <tr class="info">
-                        <th>php</th><th>1周</th><th>内容简介</th>
+                        <th>bootstrap</th><td>全世界最流行的前端框架之一，让非专业的前段也能做出酷炫的网页。您现在正在看的页面也用到了这个框架。</td>
                     </tr>
                     <tr class="success">
-                        <th>mysql</th><th>1周</th><th>内容简介</th>
+                        <th>php</th><td>让网站具备灵魂，处理业务逻辑，不在徒有其表。我们假设您是零基础，着重讲解语法和php高级特性，为后面课程打下牢固的基础。</td>
+                    </tr>
+                    <tr class="info">
+                        <th>正则表达式</th><td>字符串处理利器，采集程序，爬虫必备技术</td>
+                    </tr>
+                    <tr class="success">
+                        <th>mysql</th><td>数据是每个网站的核心部分，这里我们将详细讲解数据库的各种知识点。</td>
+                    </tr>
+                    <tr class="info">
+                        <th>redis</th><td>Key-Value数据库，讲解如何利用redis缓存数据提高并发量，解决性能问题。</td>
                     </tr>
                     <tr class="warning">
-                        <th>laravel</th><th>1周</th><th>内容简介</th>
+                        <th>laravel</th><td>前段有框架，后端自然也有，这是我们本次课程最难的部分。学会之后可以快速的开发出您想开发的任何网站。</td>
                     </tr>
                     </tbody>
                 </table>
@@ -86,16 +95,74 @@
         <div class="bg">
             <img src="{{asset('/images/php/section3.jpg')}}" />
         </div>
+        <div class="section-31">
+            <h1>开班日期:2016年06月19日(星期一)</h1>
+        </div>
+        <div class="section-33">
+            <h1>开课地点:杭州</h1>
+        </div>
+        <div class="section-32">
+            <h1>讲师:刘荣飞</h1>
+        </div>
     </div>
     <div class="section">
         <div class="bg">
             <img src="{{asset('/images/php/section4.jpg')}}" />
+        </div>
+        <div class="container">
+            <h1>4个月=16周=80天=720小时 高强度学习</h1>
+            <div class="ksap-1 table-responsive">
+                <table class="table table-bordered table-hover">
+                    <thead>
+                    <tr style="background-color:#FF8F00; color:#eee;"><th style="width:200px;">#</th><th>星期一</th><th>星期二</th><th>星期三</th><th>星期四</th><th>星期五</th><th>星期六</th><th>星期日</th></tr>
+                    </thead>
+                    <tbody>
+                    <tr class="success">
+                        <th>08:30~09:25</th><td colspan="5">讲课</td><td colspan="2" rowspan="11" class="warning" style="vertical-align: middle">放假</td>
+                    </tr>
+                    <tr class="success">
+                        <th>09:30~10:25</th><td colspan="5">练习</td>
+                    </tr>
+                    <tr class="info">
+                        <th>10:30~11:25</th><td colspan="5">讲课</td>
+                    </tr>
+                    <tr  class="info">
+                        <th>11:30~12:00</th><td colspan="5">练习</td>
+                    </tr>
+                    <tr class="warning">
+                        <th>12:00~13:30</th><td colspan="5">午餐时间</td>
+                    </tr>
+                    <tr class="success">
+                        <th>13:30~14:25</th><td colspan="5">讲课</td>
+                    </tr>
+                    <tr class="success">
+                        <th>14:30~15:25</th><td colspan="5">练习</td>
+                    </tr>
+                    <tr class="info">
+                        <th>15:30~16:25</th><td colspan="5">讲课</td>
+                    </tr>
+                    <tr class="info">
+                        <th>16:30~17:30</th><td colspan="5">练习</td>
+                    </tr>
+                    <tr class="warning">
+                        <th>17:30~19:00</th><td colspan="5">晚餐时间</td>
+                    </tr>
+                    <tr class="danger">
+                        <th>19:30~20:30</th><td colspan="5">晚自习</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
     <div class="section">
         <div class="bg">
             <img src="{{asset('/images/php/section5.jpg')}}" />
         </div>
+        <div class="slide"><h3>第二屏的第一屏</h3></div>
+        <div class="slide"><h3>第二屏的第二屏</h3></div>
+        <div class="slide"><h3>第二屏的第三屏</h3></div>
+
     </div>
     <div class="section">
         <div class="bg">
