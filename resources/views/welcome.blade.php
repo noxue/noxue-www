87cce417c4d1485397f299988502279132f7a2e2
noxue <yes@noxue.com>
2017-05-25 23:43:06 +0800

    完成php招生页面

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -36,7 +36,7 @@
         <div class="container">
             <div class="content">
                 <div class="title">不学网</div>
-                <h1>即将上线</h1>
+                <h1><a href="/page/php.html" style="color:red;">不学网-PHP线下培训，点击了解</a></h1>
                 <h1>欢迎加入QQ交流群：314747787 <a href="https://jq.qq.com/?_wv=1027&k=49GUAWO" target="_blank">点击链接加入群【不学网-编程交流】</a></h1>
                 <h1><a href="http://www.bilibili.com/video/av2427340/" target="_blank">c语言教程在线观看</a></h1>
                 <h3>点击右边链接，等待几秒后，会自动下载，下载后安装时点击第一个按钮Create即可，此版本不用自己配置环境变量。<a href="http://sourceforge.net/projects/tdm-gcc/files/TDM-GCC%20Installer/tdm-gcc-5.1.0-3.exe/download" target="_blank">GCC编译器</a></h3>
